7f3281852d1deeb05ec85eff18d716ecfcca676a
wsekta <wsekta@users.noreply.github.com>
2023-12-14 19:06:36 +0000

    deploy: c9d1836f9ec4b9b10f5e501ffbf50df69ed9d2c0

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['getcolumnnames_0',['getColumnNames',['../classorm_1_1Model.html#add17789e95193ed105ff75dacdf67ca4',1,'orm::Model']]],
-  ['gettablename_1',['getTableName',['../classorm_1_1Model.html#a358bad624914f9ceab1021839bd93cc0',1,'orm::Model']]]
+  ['gettablename_1',['getTableName',['../classorm_1_1Model.html#ad8ceaa58bf6502dac3ba1326d6660d16',1,'orm::Model']]]
 ];
