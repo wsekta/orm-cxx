1303e505f702202cd7056555d2ed33b37f8b3b16
wsekta <wsekta@users.noreply.github.com>
2024-01-09 18:52:12 +0000

    deploy: 9c48aa5b418078545aa77474f3c33cc7ffc9199d

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['getbackendtype_0',['getBackendType',['../classorm_1_1Database.html#af5bf41d4644f6d3e8835bfbad5cb07a0',1,'orm::Database']]],
-  ['getcolumnsinfo_1',['getColumnsInfo',['../classorm_1_1Model.html#ae14d8edb8bf8be19e8455387d0003c5d',1,'orm::Model']]],
-  ['getobject_2',['getObject',['../classorm_1_1Model.html#a577d30c6646c56e34c219795a97dd746',1,'orm::Model']]],
-  ['gettablename_3',['getTableName',['../classorm_1_1Model.html#a224164ab3ec52859db88e68f5b220e0e',1,'orm::Model']]]
+  ['getmodelinfo_1',['getModelInfo',['../classorm_1_1Model.html#a0738b46984022add43e8eb2b93186f1d',1,'orm::Model']]],
+  ['getobject_2',['getObject',['../classorm_1_1Model.html#a577d30c6646c56e34c219795a97dd746',1,'orm::Model']]]
 ];
