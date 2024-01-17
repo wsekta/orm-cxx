3c985e27a4e061e50387b4b1006bfdff74bfcf45
Wojciech Sekta <sekta.wojciech@gmail.com>
2024-01-18 00:56:12 +0100

    last change for ForeignModelInfo

--- include/orm-cxx/model/ModelInfo.hpp
+++ include/orm-cxx/model/ModelInfo.hpp
@@ -23,57 +23,56 @@ auto generateModelInfo() -> ModelInfo
     modelInfo.tableName = getTableName<T>();
     modelInfo.columnsInfo = getColumnsInfo<T>(modelInfo.idColumnsNames);
 
-    using model_tuple_t = std::decay_t<
-        std::invoke_result_t<
-            decltype([](auto t){return rfl::to_view(t).values();}), 
-            T>>;
-    getForeignModelInfoFromModel<T>(static_cast<model_tuple_t*>(nullptr), modelInfo);
+    using model_tuple_t =
+        std::decay_t<std::invoke_result_t<decltype([](auto t) { return rfl::to_view(t).values(); }), T>>;
+    getForeignModelInfoFromModel<T, model_tuple_t>(modelInfo);
 
     return modelInfo;
 }
 
 template <typename T, typename ModelAsTuple, std::size_t TupleSize = std::tuple_size_v<ModelAsTuple>>
-static auto getForeignModelInfoFromModel(ModelAsTuple* modelAsTuple, ModelInfo& modelInfo) -> void
+inline auto getForeignModelInfoFromModel(ModelInfo& modelInfo) -> void
 {
-    getForeignModelInfoFoldIteration<T>(modelAsTuple, std::make_index_sequence<TupleSize>{}, modelInfo);
+    getForeignModelInfoFoldIteration<T, ModelAsTuple>(std::make_index_sequence<TupleSize>{}, modelInfo);
 }
 
 template <typename T, typename ModelAsTuple, std::size_t... Is>
-static auto getForeignModelInfoFoldIteration(ModelAsTuple* modelAsTuple, std::index_sequence<Is...>,
-                                             ModelInfo& modelInfo) -> void
+inline auto getForeignModelInfoFoldIteration(std::index_sequence<Is...>, ModelInfo& modelInfo) -> void
 {
-    (getForeignModelInfoFoldIterationStep<T, Is>(modelAsTuple, modelInfo), ...);
+    (getForeignModelInfoFoldIterationStep<T, Is, ModelAsTuple>(modelInfo), ...);
 }
+
+template <typename T, typename ModelField>
+struct GetForeignModelInfoFromField
+{
+    static inline auto get(std::size_t i, ModelInfo& modelInfo) -> void
+    {
+        if constexpr (checkIfIsModelWithId<std::decay_t<ModelField>>())
+        {
+            auto name = rfl::fields<T>()[i].name();
+
+            modelInfo.columnsInfo[i].isForeignModel = true;
+
+            modelInfo.columnsInfo[i].type = ColumnType::OneToMany;
+
+            modelInfo.foreignModelsInfo[name] = generateModelInfo<ModelField>();
+        }
+    }
+};
+
+template <typename T, typename ModelField>
+struct GetForeignModelInfoFromField<T, std::optional<ModelField>>
+{
+    static inline auto get(std::size_t i, ModelInfo& modelInfo) -> void
+    {
+        GetForeignModelInfoFromField<T, ModelField>::get(i, modelInfo);
+    }
+};
 
 template <typename T, std::size_t Is, typename ModelAsTuple>
-static auto getForeignModelInfoFoldIterationStep(ModelAsTuple*,
-                                             ModelInfo& modelInfo) -> void
+inline auto getForeignModelInfoFoldIterationStep(ModelInfo& modelInfo) -> void
 {
-    using field_t = std::decay_t<
-        std::invoke_result_t<
-            decltype([](auto t){return *std::get<Is>(t);}), 
-            ModelAsTuple>>;
-    getForeignModelInfoFromField<T>(Is, static_cast<field_t*>(nullptr), modelInfo);
-}
-
-template <typename T, typename ModelField>
-static auto getForeignModelInfoFromField(std::size_t i, ModelField*, ModelInfo& modelInfo) -> void
-{
-    if constexpr (checkIfIsModelWithId<std::decay_t<ModelField>>())
-    {
-        auto name = rfl::fields<T>()[i].name();
-
-        modelInfo.columnsInfo[i].isForeignModel = true;
-        
-        modelInfo.columnsInfo[i].type = ColumnType::OneToMany;
-
-        modelInfo.foreignModelsInfo[name] = generateModelInfo<ModelField>();
-    }
-}
-
-template <typename T, typename ModelField>
-static auto getForeignModelInfoFromField(std::size_t i, std::optional<ModelField>*, ModelInfo& modelInfo) -> void
-{
-    getForeignModelInfoFromField<T>(i, static_cast<ModelField*>(nullptr), modelInfo);
+    using field_t = std::decay_t<std::invoke_result_t<decltype([](auto t) { return *std::get<Is>(t); }), ModelAsTuple>>;
+    GetForeignModelInfoFromField<T, field_t>::get(Is, modelInfo);
 }
 }
