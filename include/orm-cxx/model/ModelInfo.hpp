61dc769ab8710cc5adf21ebd8c9d58dc4704a132
Wojciech Sekta <wojciech.sekta@elzab.pl>
2024-01-17 19:12:15 +0100

    remove all unnecessary instance creation in getForeignModelInfoFromModel

--- include/orm-cxx/model/ModelInfo.hpp
+++ include/orm-cxx/model/ModelInfo.hpp
@@ -14,6 +14,13 @@ struct ModelInfo
     std::unordered_map<std::string, ModelInfo> foreignModelsInfo;
 };
 
+struct to_view_t{
+    auto operator()(const auto t) const -> decltype(auto)
+    {
+        return rfl::to_view(t).values();
+    }
+};
+
 template <typename T>
 auto generateModelInfo() -> ModelInfo
 {
@@ -23,33 +30,46 @@ auto generateModelInfo() -> ModelInfo
     modelInfo.tableName = getTableName<T>();
     modelInfo.columnsInfo = getColumnsInfo<T>(modelInfo.idColumnsNames);
 
-    T model{};
-    auto values = rfl::to_view(model).values();
-
-    //    std::invoke_result_t<rfl::to_view, T> a;
-
-    getForeignModelInfoFromModel<T>(values, modelInfo);
+    using model_tuple_t = std::decay_t<std::invoke_result_t<to_view_t, T>>;
+    getForeignModelInfoFromModel<T>(static_cast<model_tuple_t*>(nullptr), modelInfo);
 
     return modelInfo;
 }
 
 template <typename T, typename ModelAsTuple, std::size_t TupleSize = std::tuple_size_v<ModelAsTuple>>
-static auto getForeignModelInfoFromModel(ModelAsTuple& modelAsTuple, ModelInfo& modelInfo) -> void
+static auto getForeignModelInfoFromModel(ModelAsTuple* modelAsTuple, ModelInfo& modelInfo) -> void
 {
     getForeignModelInfoFoldIteration<T>(modelAsTuple, std::make_index_sequence<TupleSize>{}, modelInfo);
 }
 
 template <typename T, typename ModelAsTuple, std::size_t... Is>
-static auto getForeignModelInfoFoldIteration(ModelAsTuple& modelAsTuple, std::index_sequence<Is...>,
+static auto getForeignModelInfoFoldIteration(ModelAsTuple* modelAsTuple, std::index_sequence<Is...>,
                                              ModelInfo& modelInfo) -> void
 {
-    (getForeignModelInfoFromField<T>(Is, std::get<Is>(modelAsTuple), modelInfo), ...);
+    (getForeignModelInfoFoldIterationStep<T, Is>(modelAsTuple, modelInfo), ...);
+}
+
+template <std::size_t Is>
+struct get_t
+{
+    auto operator()(auto t) const -> decltype(auto)
+    {
+        return *std::get<Is>(t);
+    }
+};
+
+template <typename T, std::size_t Is, typename ModelAsTuple>
+static auto getForeignModelInfoFoldIterationStep(ModelAsTuple* modelAsTuple,
+                                             ModelInfo& modelInfo) -> void
+{
+    using field_t = std::decay_t<std::invoke_result_t<get_t<Is>, ModelAsTuple>>;
+    getForeignModelInfoFromField<T>(Is, static_cast<field_t*>(nullptr), modelInfo);
 }
 
 template <typename T, typename ModelField>
 static auto getForeignModelInfoFromField(std::size_t i, ModelField*, ModelInfo& modelInfo) -> void
 {
-    if constexpr (checkIfIsModelWithId<ModelField>())
+    if constexpr (checkIfIsModelWithId<std::decay_t<ModelField>>())
     {
         auto name = rfl::fields<T>()[i].name();
 
