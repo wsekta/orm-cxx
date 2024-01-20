f924e37f73fa752d4acd4a02127740c7af8c8706
Wojciech Sekta <sekta.wojciech@gmail.com>
2024-01-20 11:48:42 +0100

    fix designator order in QueryData declaration issue

--- include/orm-cxx/query/QueryData.hpp
+++ include/orm-cxx/query/QueryData.hpp
@@ -9,8 +9,8 @@ namespace orm::query
 {
 struct QueryData
 {
+    const model::ModelInfo& modelInfo;                /**< The model info for the query. */
     std::optional<std::size_t> offset = std::nullopt; /**< The optional OFFSET value for the query. */
     std::optional<std::size_t> limit = std::nullopt;  /**< The optional LIMIT value for the query. */
-    const model::ModelInfo& modelInfo;                /**< The model info for the query. */
 };
 }
