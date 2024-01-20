2d829427832af027887a0a97de2824ce02fd5079
Wojciech Sekta <sekta.wojciech@gmail.com>
2024-01-20 22:29:11 +0100

    fix tests

--- scripts/build_and_test_linux_gxx.sh
+++ scripts/build_and_test_linux_gxx.sh
@@ -6,8 +6,8 @@ cd build-linux-gxx || exit 1
 
 cmake .. -DCMAKE_BUILD_TYPE=Debug \
          -DCMAKE_CXX_COMPILER=/usr/bin/g++-13 \
-         -GNinja
+         -GNinja || exit 1
 
-cmake --build . --config Debug
+cmake --build . --config Debug || exit 1
 
 ctest -C Debug --output-on-failure
