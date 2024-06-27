24a49831525acd34ae122d37be8310b1ef19fe93
wsekta <wsekta@users.noreply.github.com>
2024-06-27 18:04:59 +0000

    deploy: 8562c31a1cd1b12f694c4fc23d483d08fc162b23

--- search/all_8.js
+++ search/all_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['mapping_20fields_20names_0',['Mapping fields&apos; names',['../md_docs_2model.html#autotoc_md11',1,'']]],
-  ['model_1',['Model',['../md_docs_2model.html#autotoc_md8',1,'Create simple model'],['../classorm_1_1Model.html',1,'orm::Model&lt; T &gt;'],['../md_docs_2model.html',1,'Model']]]
+  ['model_1',['Model',['../classorm_1_1Model.html',1,'orm::Model&lt; T &gt;'],['../md_docs_2model.html',1,'Model']]],
+  ['model_2',['Create simple model',['../md_docs_2model.html#autotoc_md8',1,'']]]
 ];
--- search/all_a.js
+++ search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['objects_0',['Objects',['../md_docs_2database.html#autotoc_md4',1,'Insert objects'],['../md_docs_2database.html#autotoc_md5',1,'Query objects']]],
+  ['objects_0',['objects',['../md_docs_2database.html#autotoc_md4',1,'Insert objects'],['../md_docs_2database.html#autotoc_md5',1,'Query objects']]],
   ['offset_1',['offset',['../classorm_1_1Query.html#ad277561652627e31187604dd6487fec8',1,'orm::Query']]],
   ['offset_20select_2',['Offset select',['../md_docs_2query.html#autotoc_md16',1,'']]],
   ['orm_20c_3',['ORM C++',['../index.html',1,'']]],
